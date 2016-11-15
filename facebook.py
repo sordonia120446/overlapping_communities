a939f8bb1bc9631ae07670206eac80715f773778
sordonia120446 <samuel.ordonia@gmail.com>
2016-11-14 16:29:24 -0800

    Added weights to graph and verified plotting with Kamada Kawaii appraoch

--- facebook.py
+++ facebook.py
@@ -13,28 +13,24 @@ Create social network graph based on Stanford's Facebook data.
 
 # Reading in the input file 
 path = os.path.abspath("references/facebook/facebook_combined.txt")
-print(path)
 file = open(path, 'r')
 my_graph = Graph.Read_Ncol(file, directed = False)
-# print(my_graph.vs["name"])
 
-# Coming up with random ages
+# Coming up with random ages, gender, and formal/informal relationships
+gender_types = ["m", "f"]
+is_formal_types = [True, False]
 for vertex in my_graph.vs:
 	vertex["age"] = round(random.uniform(1, 65))
-	print(vertex["age"])
-
-# my_graph = Graph([(0,1), (0,2), (2,3), (3,4), (4,2), (2,5), (5,0), (6,3), (5,6)])
-# my_graph.vs["age"] = [25, 31, 18, 47, 22, 23, 50]
-# my_graph.vs["gender"] = ["f", "m", "f", "m", "f", "m", "m"]
-# my_graph.es["is_formal"] = [False, False, True, True, True, False, True, False, False]
+	vertex["gender"] = gender_types[round(random.uniform(0,1))]
+	vertex["is_formal"] = is_formal_types[round(random.uniform(0,1))]
 
 # Adding weights to these edges.  Weights are determined by age deltas b/w nodes. 
-# weights = list();
-# for edge in my_graph.es:
-# 	source_vertex = my_graph.vs[edge.source]["age"]
-# 	target_vertex = my_graph.vs[edge.target]["age"]
-# 	weights.append( abs(source_vertex - target_vertex) )
-# my_graph.es["weight"] = weights
+weights = list();
+for edge in my_graph.es:
+	source_vertex = my_graph.vs[edge.source]["age"]
+	target_vertex = my_graph.vs[edge.target]["age"]
+	weights.append( abs(source_vertex - target_vertex) )
+my_graph.es["weight"] = weights
 
 # ---------------------------------------------------------------------------------------
 # Running the entire NECTAR algorithm here!  
@@ -43,7 +39,7 @@ for vertex in my_graph.vs:
 # beta = 1
 
 # # my_vertex_id = 4
-# # plot_Kamada_Kawai(my_graph)
+# plot_Kamada_Kawai(my_graph)
 
 # # Testing the entire outer_nectar algorithm.  
 # communities_per_node_from_nectar = outer_nectar(my_graph, beta)
